d993ab1b5335df672c75caa0aa673cad25a15391
Michael Bosworth <boz@protonmail.com>
2025-05-14 11:14:59 -0500

    add error boundary

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { Suspense, onMount } from "solid-js";
+import { Suspense, onMount, ErrorBoundary } from "solid-js";
 import "./app.css";
 
 export default function App() {
@@ -10,29 +10,31 @@ export default function App() {
     }
   });
   return (
-    <Router
-      root={props => (
-        <>
-          <script innerHTML={`
-            if (location.hostname === "mozworth.music") {
-              (function(){
-                var gtagScript = document.createElement('script');
-                gtagScript.async = true;
-                gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-HCSKGBDXDT";
-                document.head.appendChild(gtagScript);
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                window.gtag = gtag;
-                gtag('js', new Date());
-                gtag('config', 'G-HCSKGBDXDT');
-              })();
-            }
-          `} />
-          <Suspense>{props.children}</Suspense>
-        </>
-      )}
-    >
-      <FileRoutes />
-    </Router>
+    <ErrorBoundary fallback={err => { console.error("Global error boundary:", err); return null; }}>
+      <Router
+        root={props => (
+          <>
+            <script innerHTML={`
+              if (location.hostname === "mozworth.music") {
+                (function(){
+                  var gtagScript = document.createElement('script');
+                  gtagScript.async = true;
+                  gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-HCSKGBDXDT";
+                  document.head.appendChild(gtagScript);
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  window.gtag = gtag;
+                  gtag('js', new Date());
+                  gtag('config', 'G-HCSKGBDXDT');
+                })();
+              }
+            `} />
+            <Suspense>{props.children}</Suspense>
+          </>
+        )}
+      >
+        <FileRoutes />
+      </Router>
+    </ErrorBoundary>
   );
 }
