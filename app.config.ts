55b0fd81ce7b4fc5bb758b559ed871d183a8c9ff
Michael Bosworth <boz@protonmail.com>
2025-05-15 21:58:04 -0500

    new pwa

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "@solidjs/start/config";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss(), VitePWA()]
   }
 });
