6f91afcfbeb38b702d43d71bc637477c8c37e158
Michael Bosworth <boz@protonmail.com>
2025-07-15 16:35:33 -0500

    update tour

--- src/routes/tour.tsx
+++ src/routes/tour.tsx
@@ -23,7 +23,7 @@ export default function Tour() {
             <div class="tour-date upcoming w-full border-green-500 bg-green-500/10 border mb-6 p-6 rounded-xl flex flex-col md:flex-row items-start gap-4 md:gap-6">
               <div class="min-w-[200px] text-left font-bold">Friday, August 1, 2025</div>
               <div class="venue-info flex-1 text-left text-base">
-                <p>The Sky Is Falling Single Release Party<br />South Austin Beer Garden<br />8pm - 10pm<br />Austin, TX</p>
+                <p>The Sky Is Falling Single Release Party<br />South Austin Beer Garden<br />8pm - 10pm<br />Austin, TX<br /><span class="block mt-2 text-green-300 font-semibold">The Distraits open at 8pm</span><span class="block text-teal-300 font-semibold">mozworth plays at 9pm</span></p>
               </div>
               <div class="tour-links w-full md:w-auto md:min-w-[140px] flex flex-col md:flex-row flex-wrap gap-2 mt-4 md:mt-0 md:justify-end">
                 <a href="https://www.bandsintown.com/e/107046899?" class="tour-link w-full md:w-auto text-center inline-block border border-white/30 rounded-full px-6 py-3 md:px-4 md:py-2 text-base md:text-sm transition-all duration-200 hover:bg-white hover:text-black" target="_blank" rel="noopener">Bandsintown</a>
